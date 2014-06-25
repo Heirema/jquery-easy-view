ab93093946c97ec0f875dd51a8055b5f6e9e9fb6
Rafael Toledo <rafamd@gmail.com>
2014-06-25 11:28:05 -0300

    Salvando instância do plugin no container para reaproveitamento; implementação inicial de API

--- jquery.font-accessibility.dev.js
+++ jquery.font-accessibility.dev.js
@@ -1,162 +1,190 @@
 (function($){
 	$.fn.easyView = function(option, value){
-		var booted = false;
-		var selector = this.selector;
+		var selector = $(this.selector);
 
-		if(typeof option == 'object'){
-			/* Default Values */
-			var defaults = {
-				container: 'body',
-				tags: ['h1','h2','h3','h4','h5','h6', 'div', 'p', 'a', 'span', 'strong', 'em', 'ul', 'ol', 'li'],
-				step: 10,
-				bootstrap: true,
-				defaultMarkup: '<a href="#decrease" class="decrease-text">Decrease font size</a><a href="#normal" class="reset-text">Normal font size</a><a href="#increase" class="increase-text">Increase font size</a><a href="#contrast" class="contrast-text">Change contrast</a>',
-				increaseSelector: '.increase-text',
-				decreaseSelector: '.decrease-text',
-				normalSelector: '.reset-text',
-				contrastSelector: '.contrast-text'
+		if(typeof selector.data('easyView') == 'undefined'){
+			/* First execution */
+			if(typeof option == 'string'){
+				option = {};
+			}
+
+			var plugin = {
+				selector: selector,
+				currentRatio: 100,
+				normalContrast: true,
+				defaults: {
+					container: 'body',
+					tags: ['h1','h2','h3','h4','h5','h6', 'div', 'p', 'a', 'span', 'strong', 'em', 'ul', 'ol', 'li'],
+					step: 10,
+					bootstrap: true,
+					defaultMarkup: '<a href="#decrease" class="decrease-text">Decrease font size</a><a href="#normal" class="reset-text">Normal font size</a><a href="#increase" class="increase-text">Increase font size</a><a href="#contrast" class="contrast-text">Change contrast</a>',
+					increaseSelector: '.increase-text',
+					decreaseSelector: '.decrease-text',
+					normalSelector: '.reset-text',
+					contrastSelector: '.contrast-text'
+				},
+				options: {},
+				affectedTags: new Array(),
+				mergeOptions: function(option){
+					console.log('Merging options');
+					$.extend(this.options, this.defaults, option)
+				},
+				storeDefaults: function(){
+					console.log('Storing default values');
+					/* Store default values for each elements */
+					$(this.affectedTags.join(',')).each(function(){
+						var current_tag = $(this);
+						var font_size = current_tag.css('font-size');
+
+						if(font_size.indexOf('%') > -1){
+							/* Percentage */
+							current_tag.data('originalSize', parseInt(font_size.replace('%','')));
+							current_tag.data('originalUnit', '%');
+						} else {
+							/* Other units */
+							current_tag.data('originalSize', parseInt(font_size.replace(font_size.substr(-2),'')));
+							current_tag.data('originalUnit', font_size.substr(-2));
+						}
+
+						current_tag.data('originalBackground', current_tag.css('background-color'));
+						current_tag.data('originalColor', current_tag.css('color'));
+					});
+
+					/* Container default values */
+					$(this.options.container).data('originalBackground', $(this.options.container).css('background-color'));
+					$(this.options.container).data('originalColor', $(this.options.container).css('color'));
+				},
+				createDefaultMarkup: function(){
+					console.log('Creating default html markup');
+					/* Create a default markup */
+					if(selector.html() == ''){
+						selector.html(this.options.defaultMarkup);
+					}
+				},
+				setActions: function(){
+					console.log('Setting actions to links/buttons');
+					var self = this;
+
+					/* Decrease font size */
+					selector.find(this.options.decreaseSelector).click(function(ev){
+						ev.preventDefault();
+						self.decreaseFont();
+					});
+
+					/* Reset font size */
+					selector.find(this.options.normalSelector).click(function(ev){
+						ev.preventDefault();
+						self.resetFont();
+					});
+
+					/* Increase font size */
+					selector.find(this.options.increaseSelector).click(function(ev){
+						ev.preventDefault();
+						self.increaseFont();
+					});
+
+					/* Change text contrast */
+					selector.find(this.options.contrastSelector).click(function(ev){
+						ev.preventDefault();
+						self.changeContrast();
+					});
+				},
+				fetchTags: function(){
+					console.log('Fetching all tags');
+					/* Fetching all tags to work */
+					var affectedTags = this.affectedTags;
+					var options = this.options;
+					$.each(this.options.tags, function(i, v){
+						affectedTags.push(options.container+" "+v);
+					});
+
+					console.log(affectedTags);
+				},
+				decreaseFont: function(){
+					console.log('Decrease font size');
+
+					if((this.currentRatio - this.options.step) >= 10){
+						this.currentRatio = this.currentRatio - this.options.step;
+					}
+					this.changeFontSize();
+				},
+				resetFont: function(){
+					console.log('Reset font size');
+
+					/* Set default ratio */
+					this.currentRatio = 100;
+					this.changeFontSize();
+				},
+				increaseFont: function(){
+					console.log('Increase font size');
+
+					this.currentRatio = this.currentRatio + this.options.step;
+					this.changeFontSize();
+				},
+				changeFontSize: function(ratio){
+					console.log('Applying new font size ratio');
+					if(typeof ratio != 'undefined' && parseInt(ratio) > 10){
+						this.currentRatio = ratio;
+					}
+
+					var current_ratio = this.currentRatio;
+
+					$(this.affectedTags.join(',')).each(function(){
+						var current_tag = $(this);
+						current_tag.css('font-size', (current_tag.data('originalSize')*(current_ratio/100))+current_tag.data('originalUnit'));
+					});
+				},
+				changeContrast: function(){
+					console.log('Change content contrast');
+
+					$(this.affectedTags).each(function(){
+						var current_tag = $(this);
+						current_tag.css('background-color', (!this.normalContrast) ? current_tag.data('originalBackground') : '#000');
+						current_tag.css('color', (!this.normalContrast) ? current_tag.data('originalColor') : '#fff');
+					});
+
+					$(this.options.container).css('background-color', (!this.normalContrast) ? $(this.options.container).data('originalBackground') : '#000');
+					$(this.options.container).css('color', (!this.normalContrast) ? $(this.options.container).data('originalColor') : '#fff');
+
+					this.normalContrast = !this.normalContrast;
+				},
+				startPlugin: function(option){
+					console.log('Starting plugin');
+
+					this.mergeOptions(option);
+					this.fetchTags();
+					this.storeDefaults();
+					this.createDefaultMarkup();
+					this.setActions();
+
+					console.log('Plugin started');
+				},
+				destroy: function(){
+					console.log('Destroying plugin');
+
+					/* Back all fonts to default size */
+					this.resetFont();
+
+					/* Remove contrast change */
+					this.normalContrast = false;
+					this.changeContrast();
+
+					/* Remove plugin data */
+					selector.removeData('easyView');
+
+					console.log('Plugin destroyed');
+				}
 			};
 
-			/* Merge values */
-			var option = $.extend(defaults, option);
+			plugin.startPlugin(option);
 
-			/* Affected tags */
-			var affected_tags = new Array();
-			$.each(option.tags, function(i, value){
-				affected_tags.push(option.container+" "+value);
-			});
+			/* Store plugin instance */
+			selector.data('easyView', plugin);
 
-			affected_tags = affected_tags.join(',');
-
-			/* Text ratio - Percent */
-			var current_ratio = 100;
-			var normal_contrast = true;
-
-			setCurrentValues();
-			putAllTogether();
-		} else if(typeof option == 'string' && booted){
-			switch(option){
-				case 'toRatio':
-						doTheFontTrick(value);
-					break;
-				case 'normal':
-						normalText();
-					break;
-				case 'contrast': 
-						changeContrast();
-					break;
-				default:
-						alert("Not a valid option");
-					break;
-			}
 		} else {
-			alert("Plugin not initialized");
-		}
+			/* Plugin is already initialized, execute existing function */
+			
 
-
-		/* Store current values (font size, font color, background) */
-		function setCurrentValues(){
-			$(affected_tags).each(function(){
-				var current_tag = $(this);
-				var font_size = current_tag.css('font-size');
-
-				if(font_size.indexOf('%') > -1){
-					/* Percentage */
-					current_tag.data('originalSize', parseInt(font_size.replace('%','')));
-					current_tag.data('originalUnit', '%');
-				} else {
-					/* Other units */
-					current_tag.data('originalSize', parseInt(font_size.replace(font_size.substr(-2),'')));
-					current_tag.data('originalUnit', font_size.substr(-2));
-				}
-
-				current_tag.data('originalBackground', current_tag.css('background-color'));
-				current_tag.data('originalColor', current_tag.css('color'));
-			});
-
-			$(option.container).data('originalBackground', $(option.container).css('background-color'));
-			$(option.container).data('originalColor', $(option.container).css('color'));
-
-			booted = true;
-		}
-
-		/* Put all information together */
-		function putAllTogether(){
-			var font_utility_wrap = $(selector);
-			if(font_utility_wrap.html() == ''){
-				font_utility_wrap.html(option.defaultMarkup);
-			}
-
-			/* Decrease */
-			font_utility_wrap.find(option.decreaseSelector).click(function(ev){
-				ev.preventDefault();
-				decreaseText();
-			});
-
-			/* Reset */
-			font_utility_wrap.find(option.normalSelector).click(function(ev){
-				ev.preventDefault();
-				normalText();
-			});
-
-			/* Increase */
-			font_utility_wrap.find(option.increaseSelector).click(function(ev){
-				ev.preventDefault();
-				increaseText();
-			});
-
-			/* Contrast */
-			font_utility_wrap.find(option.contrastSelector).click(function(ev){
-				ev.preventDefault();
-				changeContrast();
-			});
-		}
-
-		/* Helper to decrease font-size */
-		function decreaseText(){
-			if((current_ratio - option.step) > 10){
-				current_ratio = current_ratio - option.step;
-			}
-
-			doTheFontTrick();
-		}
-
-		/* Helper to reset font-size */
-		function normalText(){
-			current_ratio = 100;
-			doTheFontTrick();
-		}
-
-		/* Helper to increase font-size */
-		function increaseText(){
-			current_ratio = current_ratio + option.step;
-			doTheFontTrick();
-		}
-
-		/* Apply current ratio */
-		function doTheFontTrick(ratio){
-			if(typeof ratio != 'undefined' && parseInt(ratio) > 10){
-				current_ratio = ratio;
-			}
-
-			$(affected_tags).each(function(){
-				var current_tag = $(this);
-				current_tag.css('font-size', (current_tag.data('originalSize')*(current_ratio/100))+current_tag.data('originalUnit'));
-			});
-		}
-
-		/* Change text contrast */
-		function changeContrast(){
-			$(affected_tags).each(function(){
-				var current_tag = $(this);
-				current_tag.css('background-color', (!normal_contrast) ? current_tag.data('originalBackground') : '#000');
-				current_tag.css('color', (!normal_contrast) ? current_tag.data('originalColor') : '#fff');
-			});
-
-			$(option.container).css('background-color', (!normal_contrast) ? $(option.container).data('originalBackground') : '#000');
-			$(option.container).css('color', (!normal_contrast) ? $(option.container).data('originalColor') : '#fff');
-
-			normal_contrast = !normal_contrast;
 		}
 	}
 }(jQuery));
